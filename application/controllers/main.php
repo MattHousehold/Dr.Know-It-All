2dff1b6640f753a7df5ef305cc4899832d3af34a
MattHousehold <matt.household@mail.utoronto.ca>
2014-03-22 18:59:43 -0400

    yuh

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -10,8 +10,8 @@
     {
       $data['title'] = "Dr. Know-It-All";
       $data['posts'] = $this->quote_model->getPosts();
-      $this->load->view('templates/header', $data2);
-      $this->load->view('main_view', $data2);
+      $this->load->view('templates/header', $data);
+      $this->load->view('main_view', $data);
       $this->load->view('templates/footer');
     }
   }
