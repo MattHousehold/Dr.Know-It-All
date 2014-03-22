da545f53c6441b34f976e000dfe555bd822e895b
MattHousehold <matt.household@mail.utoronto.ca>
2014-03-22 17:19:21 -0400

    22 March
    2014

--- application/views/admin_view.php
+++ application/views/admin_view.php
@@ -20,11 +20,11 @@
     <h2>Edit Posts</h2>
     <ul>
       <?php foreach($posts as $post): ?>
-        <li><a href="/~ccit1570/cct460/index.php/admin/update/<?php echo $post['id']; ?>"><?php echo $post['title']; ?></a> | <a href="/~ccit1570/cct460/index.php/admin/delete/<?php echo $post['id']; ?>">delete post</a></li>
+        <li><a href="/~ccit1570/MattFinalAssignment/index.php/admin/update/<?php echo $post['id']; ?>"><?php echo $post['title']; ?></a> | <a href="/~ccit1570/MattFinalAssignment/index.php/admin/delete/<?php echo $post['id']; ?>">delete post</a></li>
       <?php endforeach; ?>
     </ul>
 
-    <a href="/~ccit1570/cct460/index.php/blog" target="_blank"><p>View your creation!</p></a>
+    <a href="/~ccit1570/MattFinalAssignment/index.php/blog" target="_blank"><p>View your creation!</p></a>
 
   </body>
 </html>
